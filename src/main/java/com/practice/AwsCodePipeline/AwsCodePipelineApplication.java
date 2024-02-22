07b651bc3d65809496c9ff137f37efcad142c1c7
Vidyamanohar <111647648+Vidyamanohar@users.noreply.github.com>
2024-02-22 18:19:55 +0530

    first commit

---
+++ src/main/java/com/practice/AwsCodePipeline/AwsCodePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.practice.AwsCodePipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/AwsCodePipeline/AwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.AwsCodePipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
